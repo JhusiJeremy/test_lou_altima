ea22ebdd44849565a428d2b0ee6818b2532f33cd
JhusiJeremy <jeremy22484@gmail.com>
2017-03-21 11:22:26 +0800

    carousel finished

--- dist/scripts/javascript-min.js
+++ dist/scripts/javascript-min.js
@@ -1,1 +1,1 @@
-$("#video-start").click(function(){console.log("YES!"),$("#movie").get(0).play(),$("#video-start").css("visibility","hidden")});
+$("#video-start").click(function(){$("#movie").get(0).play(),$("#video-start").css("visibility","hidden")});
--- dist/scripts/javascript.js
+++ dist/scripts/javascript.js
@@ -1,7 +1,6 @@
 
 //button to start the movie
 $('#video-start').click(function(){
-  console.log('YES!');
   $('#movie').get(0).play();
   $('#video-start').css("visibility", "hidden");
 });
